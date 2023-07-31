450bfaa8284a8fded6ea2463639072ab8a2cdef2
huxiguo <huxiguo@freedotech.com>
2023-07-31 19:03:32 +0800

    change style

--- src/views/community.vue
+++ src/views/community.vue
@@ -5,18 +5,18 @@ import boxTitle from '@/components/boxTitle/index.vue'
 <template>
   <div class="h100% flex items-start justify-between">
     <div class="h90% w20% flex flex-col">
-      <div class="tm m2 h20% p4">
+      <div class="m2 h20% p4 tm">
         <!-- title -->
         <boxTitle title="安防概况" />
         <!-- chart -->
       </div>
-      <div class="tm m2 m-t0 h80%">下</div>
+      <div class="m2 m-t0 h80% tm">下</div>
     </div>
     <div class="h90% w20% flex flex-col">
-      <div class="tm mtl h10%">黑名单</div>
-      <div class="tm mtl h30%">访客</div>
-      <div class="tm mtl h30%">业主关怀</div>
-      <div class="tm mtl h30%">报警数据</div>
+      <div class="h10% tm mtl">黑名单</div>
+      <div class="h30% tm mtl">访客</div>
+      <div class="h30% tm mtl">业主关怀</div>
+      <div class="h30% tm mtl">报警数据</div>
     </div>
   </div>
 </template>
